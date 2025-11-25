52d79e590eb36c077cad1c6e6d12e53d6f3db6b5
alan-6565 <153258467+alan-6565@users.noreply.github.com>
2025-11-25 10:41:54 -0800

    Task Manager commit

---
+++ src/main/java/com/pluralsight/TaskManager.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+import java.util.*;
+public class TaskManager {
+    private List<Task> tasks = new ArrayList<>();
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
+    public boolean markTaskDone(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).setDone(true);
+            return true;
+        }
+        return false;
+    }
+    public boolean deleteTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+            return true;
+        }
+        return false;
+    }
+}
