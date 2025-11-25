80fce7ea103c2b6998097f6380b9018b19a0d452
RobertoGG <rguerreroyu@gmail.com>
2025-11-25 09:59:48 -0800

    Task class made

---
+++ src/main/java/com/pluralsight/Task.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Task {
+        private String title;
+        private String description;
+        private boolean isDone;
+        public Task(String title, String description) {
+            this.title = title;
+            this.description = description;
+            this.isDone = false;
+        }
+        public String getTitle() { return title; }
+        public String getDescription() { return description; }
+        public boolean isDone() { return isDone; }
+        public void setDone(boolean done) { this.isDone = done; }
+        @Override
+        public String toString() {
+            return (isDone ? "[X] " : "[ ] ") + title + ": " + description;
+        }
+}
